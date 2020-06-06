c6c3b23b73729dae72e48860a397eafd8b8a173b
SsssshiFT-96 <zhanzj96@163.com>
2020-06-06 20:46:40 +0800

    整合MyBatis，配置数据库，将用户信息存入数据库中

--- src/main/java/com/stndorm/community/controller/AuthorizeController.java
+++ src/main/java/com/stndorm/community/controller/AuthorizeController.java
@@ -2,12 +2,17 @@ package com.stndorm.community.controller;
 
 import com.stndorm.community.dto.AccessTokenDTO;
 import com.stndorm.community.dto.GithubUser;
+import com.stndorm.community.mapper.UserMapper;
+import com.stndorm.community.model.User;
 import com.stndorm.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 /**
  * 登录后获取code和state参数
@@ -26,10 +31,14 @@ public class AuthorizeController {
     @Value("${github.client.secret}")
     private String ClientSecret;
 
+    @Autowired
+    private UserMapper userMapper;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
-                           @RequestParam(name="state")String state){
+                           @RequestParam(name="state")String state,
+                           HttpServletRequest request){
+        //当把HttpServletRequest写在方法中时，Spring就会自动把上下文中的request放入其中
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri(ClientUri);
@@ -41,6 +50,23 @@ public class AuthorizeController {
         //获取user信息
         GithubUser user = githubProvider.getUser(accessToken);
         System.out.println(user.getId());
-        return "index";
+
+        if(user != null){
+            //将登录的用户存入数据库中
+            User user1 = new User();
+            user1.setToken(UUID.randomUUID().toString());
+            user1.setAccountId(String.valueOf(user.getId()));
+            user1.setName(user.getName());
+            user1.setGmtCreate(System.currentTimeMillis());
+            user1.setGmtModified(user1.getGmtCreate());
+            userMapper.insert(user1);
+            //登录成功，写cookie和session
+            //获取Session并写入user,如果不指定，会自动生成一个cookie
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+        }else{
+            //登录失败，重新登录
+            return "redirect:/";
+        }
     }
 }
---
+++ src/main/java/com/stndorm/community/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.stndorm.community.mapper;
+
+import com.stndorm.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+
+    @Insert("insert into users(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    public void insert(User user);
+}
