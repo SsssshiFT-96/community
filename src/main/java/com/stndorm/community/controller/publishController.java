70eb1982d01e3708e04faa653e77edef9b1ce173
SsssshiFT-96 <zhanzj96@163.com>
2020-06-07 09:28:54 +0800

    使用Bootstrap编写发布页面

---
+++ src/main/java/com/stndorm/community/controller/publishController.java
@@ -0,0 +1,13 @@
+package com.stndorm.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class publishController {
+
+    @GetMapping("/publish")
+    public String publish(){
+        return "publish";
+    }
+}
