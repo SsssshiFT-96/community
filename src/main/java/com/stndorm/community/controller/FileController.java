9c68fcbb131c4db56a138258248b5b6800ffd2fc
SsssshiFT-96 <zhanzj96@163.com>
2020-06-11 10:47:26 +0800

    利用markdown进行图片上传和修复多人登录的一些问题

---
+++ src/main/java/com/stndorm/community/controller/FileController.java
@@ -0,0 +1,18 @@
+package com.stndorm.community.controller;
+
+import com.stndorm.community.dto.FileDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FileController {
+    @RequestMapping("/file/upload")
+    @ResponseBody
+    public FileDTO upload(){
+        FileDTO fileDTO = new FileDTO();
+        fileDTO.setSuccess(1);
+        fileDTO.setUrl("/img/18jin.png");
+        return fileDTO;
+    }
+}
